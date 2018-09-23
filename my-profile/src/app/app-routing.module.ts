9dfeeef14d3ff2fbc0e96f038889dcd8b56dcf1b
Parth Natu <parth.natu01@gmail.com>
2018-09-24 00:10:58 +0530

    6th commit

--- my-profile/src/app/app-routing.module.ts
+++ my-profile/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import {HomeComponent} from './home/home.component';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full'},
+  { path: 'index',
+    children: [ {path: '', component: HomeComponent , pathMatch: 'full'},
+    {
+      path: 'home', component: HomeComponent
+    },
+    {path: 'about' , component: AboutComponent},
+     {path: 'home/home', component: AboutComponent}
+   ]
+  }
+];
 @NgModule({
+  imports: [RouterModule.forRoot(routes)],
   exports: [ RouterModule ]
 })
+
 export class AppRoutingModule {}
--- my-profile/src/app/app.module.ts
+++ my-profile/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
